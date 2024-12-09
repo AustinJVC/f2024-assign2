26994e4c15100d36f83a448cf5d52bbe1b26034a
AustinJVC <avand955@mtroyal.ca>
2024-12-09 01:01:47 -0700

    maybe?

--- scripts/tailwindScripts.js
+++ scripts/tailwindScripts.js
@@ -48,7 +48,7 @@ function generateTableStyling(table){
 
 function generateTDStyling(td){
     td.classList.add('pl-[8px]')
-    td.classList.add('pt-[10px]')
+    td.classList.add('pt-[6px]')
     if(td.querySelector('a') != null){
         td.classList.add('cursor-pointer');
         td.classList.add('underline')
