d4860cc26d1398b5e4738fd596fed4c0551f3874
AustinJVC <avand955@mtroyal.ca>
2024-12-09 01:43:03 -0700

    no response

--- scripts/tailwindScripts.js
+++ scripts/tailwindScripts.js
@@ -5,12 +5,11 @@ function generateModalStyling(modal, overlay, modalContent, closeButton, results
     modal.classList.add('-translate-x-1/2');
     modal.classList.add('-translate-y-1/2');
     modal.classList.add('bg-white');
-    modal.classList.add('p-5');
+    modal.classList.add('p-10');
     modal.classList.add('rounded-md');
     modal.classList.add('z-10');
     modal.classList.add('w-[50vw]');
     modal.classList.add('h-[50vh]');
-
     overlay.classList.add('fixed'); 
     overlay.classList.add('top-0'); 
     overlay.classList.add('left-0'); 
@@ -21,43 +20,36 @@ function generateModalStyling(modal, overlay, modalContent, closeButton, results
     modalContent.classList.add('modal-content');
     modalContent.classList.add('grid');
     modalContent.classList.add('grid-cols-[1fr_1fr]');
-
     closeButton.classList.add('absolute');
     closeButton.classList.add('top-3');
     closeButton.classList.add('right-3');
     closeButton.classList.add('text-2xl');
     closeButton.classList.add('cursor-pointer');
-
     resultsTitle.classList.add("text-2xl")
-
-    resultsSection.classList.add('h-[40%]');
+    resultsSection.classList.add('max-h-96');
     resultsSection.classList.add('overflow-y-auto');
-
     h2.classList.add("text-2xl")
     
 }
-
 function generateTableStyling(table){
     table.classList.add('border')
     table.classList.add('border-black')
     table.classList.add('border-w-10')
+    table.classList.add('w-full')
     table.classList.add('table-auto')
     
 }
-
 function generateTDStyling(td){
-    td.classList.add('px-[8px]')
-    td.classList.add('pt-[6px]')
+    td.classList.add('pl-[8px]')
+    td.classList.add('pt-[10px]')
     if(td.querySelector('a') != null){
         td.classList.add('cursor-pointer');
         td.classList.add('underline')
     }
 }
-
 function generateTRStyling(tr){
     tr.classList.add('even:bg-red-100')
 }
-
 function generateTHStyling(th){
     th.classList.add('text-left')
     th.classList.add('pl-[8px]')
