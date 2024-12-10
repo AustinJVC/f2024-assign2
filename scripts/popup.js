1cb04a784deb8fd4bdbcb18f9ee390ed44d20cd6
AustinJVC <avand955@mtroyal.ca>
2024-12-09 17:58:28 -0700

    idk

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,261 +1,289 @@
-function generatePopup(type, ref, season){
-    if(type == 'driver'){
+function generatePopup(type, ref, season) {
+    if (type == 'driver') {
         driverPopup(ref, season);
     }
-    else if(type == 'constructor'){
+    else if (type == 'constructor') {
         constructorPopup(ref, season);
-    } 
+    }
 }
 
-function driverPopup(ref, season){
+function driverPopup(ref, season) {
     driverStats = getData('https://www.randyconnolly.com/funwebdev/3rd/api/f1/drivers.php?ref=' + ref)
-    .then( (driverStats) => {
+        .then((driverStats) => {
 
-        stats = JSON.parse(driverStats);
-        const modal = document.createElement('section');
+            stats = JSON.parse(driverStats);
+            const modal = document.createElement('section');
 
-        document.body.appendChild(modal);
+            document.body.appendChild(modal);
 
-        const overlay = document.createElement('div');
+            const overlay = document.createElement('div');
 
-        document.body.appendChild(overlay); 
+            document.body.appendChild(overlay);
 
-        const modalContent = document.createElement('div');
-        modal.appendChild(modalContent);
+            const modalContent = document.createElement('div');
+            modal.appendChild(modalContent);
 
-        const closeButton = document.createElement('button');
+            const closeButton = document.createElement('button');
 
-        closeButton.textContent = 'X';
-        modalContent.appendChild(closeButton);
+            closeButton.textContent = 'X';
+            modalContent.appendChild(closeButton);
 
-        const driverInfo = document.createElement('div');
-        h2 = document.createElement('h2');
-        h2.textContent = 'Driver Details:'
-        driverInfo.appendChild(h2);
-        
-
-
-        const imgDiv = document.createElement('div');
-        driverInfo.appendChild(imgDiv);
-
-        const details = document.createElement('ul');
-
-        let li = document.createElement('li');
-        li.textContent = stats['forename'] + ' ' +stats['surname'];
-        details.appendChild(li);
-
-        let li2 = document.createElement('li');
-        li2.textContent = stats['dob'];
-        details.appendChild(li2);
-
-        let li3 = document.createElement('li');
-        li3.textContent = stats['nationality'];
-        details.appendChild(li3);
-
-        let li4 = document.createElement('li');
-        li4.textContent = stats['url'];
-        details.appendChild(li4);
-
-        driverInfo.appendChild(details);
-        modalContent.appendChild(driverInfo);
-
-        const resultsSection = document.createElement('div');
-
-        let resultsTitle = document.createElement('h2');
-        resultsTitle.textContent = "Driver Results";
-        resultsSection.appendChild(resultsTitle);
-
-        let favouriteButton = document.createElement('button');
-        favouriteButton.classList.add('fa-regular');
-        favouriteButton.classList.add('fa-heart');
-        resultsSection.appendChild(favouriteButton);
+            const driverInfo = document.createElement('div');
+            h2 = document.createElement('h2');
+            h2.textContent = 'Driver Details:'
+            driverInfo.appendChild(h2);
 
 
-        const resultsTable = document.createElement('table');
-        generateTableStyling(resultsTable)
 
-        const headerRow = document.createElement('tr');
-        const headers = ['Rnd', 'Name', 'Pos', 'Points'];
-        
-        for(let header of headers) {
-            let th = document.createElement('th');
-            th.textContent = header;
-            generateTHStyling(th);
-            headerRow.appendChild(th);
-        };
-        
-        resultsTable.appendChild(headerRow);
+            const imgDiv = document.createElement('div');
+            driverInfo.appendChild(imgDiv);
+
+            const details = document.createElement('ul');
+
+            let li = document.createElement('li');
+            li.textContent = stats['forename'] + ' ' + stats['surname'];
+            details.appendChild(li);
+
+            let li2 = document.createElement('li');
+            li2.textContent = stats['dob'];
+            details.appendChild(li2);
+
+            let li3 = document.createElement('li');
+            li3.textContent = stats['nationality'];
+            details.appendChild(li3);
+
+            let li4 = document.createElement('li');
+            li4.textContent = stats['url'];
+            details.appendChild(li4);
+
+            driverInfo.appendChild(details);
+            modalContent.appendChild(driverInfo);
+
+            const resultsSection = document.createElement('div');
+
+            let resultsTitle = document.createElement('h2');
+            resultsTitle.textContent = "Driver Results";
+            resultsSection.appendChild(resultsTitle);
+
+            let favouriteButton = document.createElement('a');
+            favouriteButton.dataset.ref = ref;
+            let icon = document.createElement('i')
+
+            icon.classList.add('fa-regular', 'fa-heart')
+
+            favouriteButton.classList.add('inline-block');
+            favouriteButton.classList.add('p-5');
+
+            favouriteButton.appendChild(icon);
+            driverInfo.appendChild(favouriteButton);
 
 
-        resultsData = JSON.parse(localStorage.getItem('resultsData' + season));
+            const resultsTable = document.createElement('table');
+            generateTableStyling(resultsTable)
 
-        for(let result of resultsData){
-            if(result.driver.ref == ref && result.race.year == season){
-                const row = document.createElement('tr');
+            const headerRow = document.createElement('tr');
+            const headers = ['Rnd', 'Name', 'Pos', 'Points'];
 
-                let round = document.createElement('td')
-                round.textContent = result.race.round 
-                generateTDStyling(round);
-                row.appendChild(round)
+            for (let header of headers) {
+                let th = document.createElement('th');
+                th.textContent = header;
+                generateTHStyling(th);
+                headerRow.appendChild(th);
+            };
 
-                let circuitName = document.createElement('td')
-                circuitName.textContent = result.race.name;
-                generateTDStyling(circuitName);
-                row.appendChild(circuitName)
+            resultsTable.appendChild(headerRow);
 
-                let pos = document.createElement('td')
-                pos.textContent = result.position 
-                generateTDStyling(pos);
-                row.appendChild(pos)
 
-                let point = document.createElement('td');
-                point.textContent = result.points;
-                generateTDStyling(point);
-                row.appendChild(point);
+            resultsData = JSON.parse(localStorage.getItem('resultsData' + season));
 
-                generateTRStyling(row);
-                resultsTable.appendChild(row);
+            for (let result of resultsData) {
+                if (result.driver.ref == ref && result.race.year == season) {
+                    const row = document.createElement('tr');
+
+                    let round = document.createElement('td')
+                    round.textContent = result.race.round
+                    generateTDStyling(round);
+                    row.appendChild(round)
+
+                    let circuitName = document.createElement('td')
+                    circuitName.textContent = result.race.name;
+                    generateTDStyling(circuitName);
+                    row.appendChild(circuitName)
+
+                    let pos = document.createElement('td')
+                    pos.textContent = result.position
+                    generateTDStyling(pos);
+                    row.appendChild(pos)
+
+                    let point = document.createElement('td');
+                    point.textContent = result.points;
+                    generateTDStyling(point);
+                    row.appendChild(point);
+
+                    generateTRStyling(row);
+                    resultsTable.appendChild(row);
+                }
             }
-        }
-        
-        resultsSection.appendChild(resultsTable);
-        modalContent.appendChild(resultsSection);
 
-        closeButton.addEventListener('click', () =>{
-            modal.style.display = 'none';
-            overlay.style.display = 'none';    
+            resultsSection.appendChild(resultsTable);
+            modalContent.appendChild(resultsSection);
+
+            closeButton.addEventListener('click', () => {
+                modal.style.display = 'none';
+                overlay.style.display = 'none';
+            })
+
+            generateModalStyling(modal, overlay, modalContent, closeButton, resultsTitle, resultsSection, h2)
+
+            modal.style.display = 'block';
+            overlay.classList.display = 'block';
         })
-        
-        generateModalStyling(modal, overlay, modalContent, closeButton, resultsTitle, resultsSection, h2)
-
-        modal.style.display = 'block';
-        overlay.classList.display = 'block'; 
-    })
 };
 
 
-function constructorPopup(ref, season){
+function constructorPopup(ref, season) {
     let constructorStats = getData('https://www.randyconnolly.com/funwebdev/3rd/api/f1/constructors.php?ref=' + ref)
-    .then( (constructorStats) => {
+        .then((constructorStats) => {
 
-        stats = JSON.parse(constructorStats);
-        const modal = document.createElement('section');
+            stats = JSON.parse(constructorStats);
+            const modal = document.createElement('section');
 
-        document.body.appendChild(modal);
+            document.body.appendChild(modal);
 
-        const overlay = document.createElement('div');
+            const overlay = document.createElement('div');
 
-        document.body.appendChild(overlay); 
+            document.body.appendChild(overlay);
 
-        const modalContent = document.createElement('div');
-        modal.appendChild(modalContent);
+            const modalContent = document.createElement('div');
+            modal.appendChild(modalContent);
 
-        const closeButton = document.createElement('button');
+            const closeButton = document.createElement('button');
 
-        closeButton.textContent = 'X';
-        modalContent.appendChild(closeButton);
+            closeButton.textContent = 'X';
+            modalContent.appendChild(closeButton);
 
-        const constInfo = document.createElement('div');
-        h2 = document.createElement('h2');
-        h2.textContent = 'Constructor Details:'
-        constInfo.appendChild(h2);
-        
-
-
-        const imgDiv = document.createElement('div');
-        constInfo.appendChild(imgDiv);
-
-        const details = document.createElement('ul');
-
-        let li = document.createElement('li');
-        li.textContent = stats.name;
-        details.appendChild(li);
+            const constInfo = document.createElement('div');
+            h2 = document.createElement('h2');
+            h2.textContent = 'Constructor Details:'
+            constInfo.appendChild(h2);
 
 
-        let li3 = document.createElement('li');
-        li3.textContent = stats['nationality'];
-        details.appendChild(li3);
+            let favouriteButton = document.createElement('a');
+            favouriteButton.dataset.ref = ref;
+            let icon = document.createElement('i')
 
-        let li4 = document.createElement('li');
-        li4.textContent = stats['url'];
-        details.appendChild(li4);
+            icon.classList.add('fa-regular', 'fa-heart')
 
-        constInfo.appendChild(details);
-        modalContent.appendChild(constInfo);
+            favouriteButton.classList.add('inline-block');
+            favouriteButton.classList.add('p-5');
 
-        const resultsSection = document.createElement('div');
-
-        let resultsTitle = document.createElement('h2');
-        resultsTitle.textContent = "Constructor Results";
-        resultsSection.appendChild(resultsTitle);
-
-        const resultsTable = document.createElement('table');
-        generateTableStyling(resultsTable)
+            favouriteButton.appendChild(icon);
+            constInfo.appendChild(favouriteButton);
 
 
+            const imgDiv = document.createElement('div');
+            constInfo.appendChild(imgDiv);
 
-        const headerRow = document.createElement('tr');
-        
-        const headers = ['Rnd', 'Circuit', 'Driver Name', 'Pos', 'Points'];
-        
-        for(let header of headers) {
-            let th = document.createElement('th');
-            th.textContent = header;
-            generateTHStyling(th);
-            headerRow.appendChild(th);
-        };
-        
-        resultsTable.appendChild(headerRow);
+            const details = document.createElement('ul');
+
+            let li = document.createElement('li');
+            li.textContent = stats.name;
+            details.appendChild(li);
 
 
-        resultsData = JSON.parse(localStorage.getItem('resultsData' + season));
+            let li3 = document.createElement('li');
+            li3.textContent = stats['nationality'];
+            details.appendChild(li3);
 
-        for(let result of resultsData){
-            if(result.constructor.ref == ref && result.race.year == season){
-                const row = document.createElement('tr');
+            let li4 = document.createElement('li');
+            li4.textContent = stats['url'];
+            details.appendChild(li4);
 
-                let round = document.createElement('td')
-                round.textContent = result.race.round 
-                generateTDStyling(round);
-                row.appendChild(round)
+            constInfo.appendChild(details);
+            modalContent.appendChild(constInfo);
 
-                let circuitName = document.createElement('td')
-                circuitName.textContent = result.race.name;
-                generateTDStyling(circuitName);
-                row.appendChild(circuitName)
+            const resultsSection = document.createElement('div');
 
-                let driverName = document.createElement('td')
-                driverName.textContent = result.driver.forename + " " + result.driver.surname;
-                generateTDStyling(driverName);
-                row.appendChild(driverName)
+            let resultsTitle = document.createElement('h2');
+            resultsTitle.textContent = "Constructor Results";
+            resultsSection.appendChild(resultsTitle);
 
-                let pos = document.createElement('td')
-                pos.textContent = result.position
-                generateTDStyling(pos); 
-                row.appendChild(pos)
+            const resultsTable = document.createElement('table');
+            generateTableStyling(resultsTable)
 
-                let point = document.createElement('td');
-                point.textContent = result.points;
-                generateTDStyling(point);
-                row.appendChild(point);
 
-                generateTRStyling(row);
-                resultsTable.appendChild(row);
+
+            const headerRow = document.createElement('tr');
+
+            const headers = ['Rnd', 'Circuit', 'Driver Name', 'Pos', 'Points'];
+
+            for (let header of headers) {
+                let th = document.createElement('th');
+                th.textContent = header;
+                generateTHStyling(th);
+                headerRow.appendChild(th);
+            };
+
+            resultsTable.appendChild(headerRow);
+
+
+            resultsData = JSON.parse(localStorage.getItem('resultsData' + season));
+
+            for (let result of resultsData) {
+                if (result.constructor.ref == ref && result.race.year == season) {
+                    const row = document.createElement('tr');
+
+                    let round = document.createElement('td')
+                    round.textContent = result.race.round
+                    generateTDStyling(round);
+                    row.appendChild(round)
+
+                    let circuitName = document.createElement('td')
+                    circuitName.textContent = result.race.name;
+                    generateTDStyling(circuitName);
+                    row.appendChild(circuitName)
+
+                    let driverName = document.createElement('td')
+                    driverName.textContent = result.driver.forename + " " + result.driver.surname;
+                    generateTDStyling(driverName);
+                    row.appendChild(driverName)
+
+                    let pos = document.createElement('td')
+                    pos.textContent = result.position
+                    generateTDStyling(pos);
+                    row.appendChild(pos)
+
+                    let point = document.createElement('td');
+                    point.textContent = result.points;
+                    generateTDStyling(point);
+                    row.appendChild(point);
+
+                    generateTRStyling(row);
+                    resultsTable.appendChild(row);
+                }
             }
-        }
 
-        resultsSection.appendChild(resultsTable);
-        modalContent.appendChild(resultsSection);
+            resultsSection.appendChild(resultsTable);
+            modalContent.appendChild(resultsSection);
 
-        closeButton.addEventListener('click', () =>{
-            modal.style.display = 'none';
-            overlay.style.display = 'none';    
-        })
+            closeButton.addEventListener('click', () => {
+                modal.style.display = 'none';
+                overlay.style.display = 'none';
+            })
 
-        generateModalStyling(modal, overlay, modalContent, closeButton, resultsTitle, resultsSection, h2)
+            favouriteButton.addEventListener('click', (event) => {
+                if (localStorage.getItem('favourites') == null) {
+                    favourites = [];
+                    favourites.push(event.target.dataset.ref)
+                    localStorage.setItem('favourites', JSON.stringify(favourites))
+                }
+            })
 
-        modal.style.display = 'block';
-        overlay.classList.display = 'block'; 
-    });
+            generateModalStyling(modal, overlay, modalContent, closeButton, resultsTitle, resultsSection, h2)
+
+            modal.style.display = 'block';
+            overlay.classList.display = 'block';
+        });
 };
+
 
